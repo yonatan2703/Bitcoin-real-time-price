6b3bb00f9d557ca5cc17cc9300ca18247f824ded
Yonatan Barhen <70335092+yonatan2703@users.noreply.github.com>
2021-11-06 23:58:59 +0200

    WIP - finished the chart component

--- src/components/BtcChart.jsx
+++ src/components/BtcChart.jsx
@@ -7,15 +7,38 @@ import {
 	CartesianGrid,
 	Tooltip,
 	ResponsiveContainer,
+	ReferenceLine,
 } from "recharts";
-import {} from "@chakra-ui/react";
+import { Spinner } from "@chakra-ui/react";
 
 const BtcChart = ({ data }) => {
-	useEffect(() => {
-		console.log(data);
-	}, []);
+	const [minClose, setMinClose] = useState();
+	const [maxClose, setMaxClose] = useState();
+	const [avgClose, setAvgClose] = useState();
+	const [refLine, setRefLine] = useState();
+	const [strokeColor, setStrokeColor] = useState();
 
-	return (
+	useEffect(() => {
+		if (data) {
+			setMinClose(Math.min(...data.map(({ Close }) => Close)));
+			setMaxClose(Math.max(...data.map(({ Close }) => Close)));
+			setRefLine(data[data.length - 1].Close);
+		}
+	}, [data]);
+
+	useEffect(() => {
+		if (minClose && maxClose)
+			setAvgClose(minClose + (maxClose - minClose) / 2);
+	}, [minClose, maxClose]);
+
+	useEffect(() => {
+		if (avgClose && refLine) {
+			if (refLine > avgClose) setStrokeColor("green");
+			else setStrokeColor("red");
+		}
+	}, [avgClose, refLine]);
+
+	return strokeColor ? (
 		<div style={{ width: "100%", height: 400 }}>
 			<ResponsiveContainer>
 				<AreaChart
@@ -23,10 +46,30 @@ const BtcChart = ({ data }) => {
 					margin={{
 						top: 10,
 						right: 30,
-						left: 0,
+						left: 11,
 						bottom: 0,
 					}}
 				>
+					<defs>
+						<linearGradient
+							id="colorUv"
+							x1="0"
+							y1="0"
+							x2="0"
+							y2="1"
+						>
+							<stop
+								offset="5%"
+								stopColor="#7EABED"
+								stopOpacity={0.7}
+							/>
+							<stop
+								offset="95%"
+								stopColor="#7EABED"
+								stopOpacity={0.2}
+							/>
+						</linearGradient>
+					</defs>
 					<CartesianGrid strokeDasharray="3 3" />
 					<XAxis dataKey="Date" />
 					<YAxis
@@ -35,22 +78,34 @@ const BtcChart = ({ data }) => {
 						allowDataOverflow={true}
 					/>
 					<Tooltip />
+					{strokeColor && (
+						<ReferenceLine
+							y={refLine}
+							label=""
+							stroke={strokeColor}
+							strokeDasharray="4 4"
+						/>
+					)}
 					<Area
 						type="monotone"
 						dataKey="Close"
-						stroke="#8884d8"
-						fill="#8884d8"
+						stroke="#7EABED"
+						fillOpacity={1}
+						fill="url(#colorUv)"
 					/>
 				</AreaChart>
 			</ResponsiveContainer>
 		</div>
+	) : (
+		<Spinner
+			thickness="5px"
+			speed="0.65s"
+			emptyColor="gray.200"
+			color="blue.500"
+			size="xl"
+			className="absolute-centered"
+		/>
 	);
 };
 
 export default BtcChart;
-
-// {
-// 	data?.map((ele) => {
-// 		return <div>{ele.Date}</div>;
-// 	});
-// }
